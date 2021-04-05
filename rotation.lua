353a3699a19088905638e303b688826a19197ab1
Björn Büttner <Idrinth@users.noreply.github.com>
2021-04-05 03:23:38 +0200

    only spam in combat + more party and party size options

--- rotation.lua
+++ rotation.lua
@@ -6,24 +6,25 @@ local function log(message)
     TextLogAddEntry("Chat", SystemData.ChatLogFilters.SAY, towstring(message))
 end
 local function slash(input)
-    local mode,ability = input:match("^([a-z1-6]+) \"([A-Za-z0-9 ]+)\"")
+    local mode,ability = input:match("^([a-z1-6]+) ([A-Za-z0-9]+)")
     if not ability or not mode then
         log("You need to set ability("..tostring(ability)..") and mode("..tostring(mode)..")!")
         return
-    elseif mode:match("^[pb][1-6]$") then
+    elseif mode:match("^[1-6][1-6]$") then
         local frequency = input:match(" ([0-9]+)$")
         frequency = tonumber(frequency)
         if frequency < 1 or frequency > 60 then
             log("Frequency out of bounds(1-60)")
             return
         end
-        local target, chars = mode:match("^[pb][1-6]$")
+        local groups, chars = mode:match("^([1-6])([1-6])$")
         log("Ability "..ability.." turned on!")
-        target = target=="b"
+        groups = tonumber(groups)
+        chars = tonumber(chars)
         abilities[ability] = {
-            frequency=frequency,
-            mode=tonumber(mode),
-            isBand=target,
+            frequency=math.ceil(frequency/chars/groups),
+            chars=chars,
+            groups=groups,
             counter=0,
             now=0,
         }
@@ -40,17 +41,25 @@ function Rotation.OnUpdate(elapsed)
         return
     end
     time = time - 1
+    if not GameData.Player.inCombat then
+        for ability,settings in pairs(abilities) do
+            settings.now = 0
+            settings.counter = 0
+        end
+        return
+    end
     for ability,settings in pairs(abilities) do
         settings.now = settings.now + 1
-        if settings.now % settings.frequency == 0 then
-            local person = settings.counter%settings.mode + 1
-            local party = math.floor(settings.counter%(settings.mode * 4)/settings.mode)+1
-            if settings.isBand then
-                AutoChannel.sendChatBand(ability..": Party "..tostring(party).." Person "..tostring(person))
+        if settings.now == settings.frequency then
+            local person = settings.counter%settings.chars + 1
+            local party = math.floor(settings.counter%settings.chars * settings.groups)/settings.chars+1
+            if settings.groups > 1 and (IsWarBandActive() or AutoChannel.isScenario()) then
+                AutoChannel.sendChatBandSay(ability..": Party "..tostring(party).." Person "..tostring(person))
             else
-                AutoChannel.sendChatParty(ability..": Person "..tostring(person))
+                AutoChannel.sendChatPartySay(ability..": Person "..tostring(person))
             end
             settings.counter = settings.counter + 1
+            settings.now = 0
             return
         end
     end
